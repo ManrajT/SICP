838c70383198f799e71880c43577a73c791826e4
Tatla <manrt@amazon.com>
2019-01-14 17:28:18 -0500

    add exercise 1.15

---
+++ chapter1/Exercise 1.15.rkt
@@ -0,0 +1,5 @@
+#lang sicp
+
+a.  It is applied 5 times
+b.  The size of problem needs to triple before memory and steps will increase by one.  This means size of problem needs to become 9x more before memory and steps
+required double.  This means order of growth for space and time is log a to base 3, or just log n.   It is a linear process, not tree recursive. 
