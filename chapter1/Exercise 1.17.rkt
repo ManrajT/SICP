acc67c0199959e15b2342be9c89f5c73ce874a62
Tatla <manrt@amazon.com>
2019-01-23 09:53:48 -0500

    adds exercise 1.17

--- chapter1/Exercise 1.17.rkt
+++ chapter1/Exercise 1.17.rkt
@@ -1,1 +1,24 @@
 #lang sicp 
+
+(define (double a)
+  (+ a a) )
+
+(define (half a)
+  (/ a 2) )
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+
+
+(define (fast-multiply b n)
+   (define (fast-multiply-iter b n a)
+     (cond ((= n 0) a)
+           ((even? n) (fast-multiply-iter (double b) (half n) a))
+           (else (fast-multiply-iter b (- n 1) (+ a b) ) )
+           ))
+  (fast-multiply-iter b n 0))
+
+
+
+
